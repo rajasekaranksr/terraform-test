a09253ddba40f35a5bffcecbaec769a110a4bded
Lingeswaran R <lingeshwaran.rangasamy@gmail.com>
2022-01-14 22:31:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-}
-
 provider "aws" {
   profile = "default"
   region  = "us-east-2"

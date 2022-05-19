d1fb89e4785c8d1a056cd3bfd4ecf61602fe0f2f
Lingeswaran R <lingeshwaran.rangasamy@gmail.com>
2022-05-19 22:32:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,9 @@
-provider "aws" {
-  region  = "us-east-2"
-  profile ="ansible"
-  #shared_credentials_file = "/home/jenkins/.aws/credentials"
-}
-
-resource "aws_instance" "vm-web" {
+resource "aws_instance" "main" {
   ami           = "ami-001089eb624938d9f"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "server for web"
+    Name = "IaC-Test"
     Env = "dev"
   }
 }

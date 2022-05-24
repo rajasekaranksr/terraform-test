ccb2e06c3fc3002797ee1e0a55d44f1dca5438e7
Lingeswaran R <lingeshwaran.rangasamy@gmail.com>
2022-05-24 17:00:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-  bucket = "terraform-state"
+  bucket = "ua-terraform-state"
   key = "dev/ec2-app2/terraform.tfstate"
   region = "us-east-2"
   }

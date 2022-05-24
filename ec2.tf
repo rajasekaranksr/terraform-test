03adab0f953675bf96de98c97777545adc527c6e
Lingeswaran R <lingeshwaran.rangasamy@gmail.com>
2022-05-24 17:10:27 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "main" {
   ami           = "ami-001089eb624938d9f"
   instance_type = "t2.micro"
-
+  count         = 1
+  
   tags = {
     Name = "IaC-Test"
     Env = "dev"
